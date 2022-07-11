7475feaad43d8f3b6754d7b85944dfd790405940
thayron.matos <thayron.matos@soluti.com.br>
2022-07-11 07:20:12 -0300

    class - configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
       <h1>hello world</h1>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
-    </div>
-  )
+    </ThemeProvider>
+  );
 }
