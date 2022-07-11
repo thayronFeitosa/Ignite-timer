21bab318b07c365c19ec594961491eba90e22175
thayron.matos <thayron.matos@soluti.com.br>
2022-07-11 08:01:02 -0300

    Class - Estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  body {
+    background: #333;
+    color: #FFF
+  }
+
+  
+`;
