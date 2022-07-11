e2fa3837c998b2e1d5d0775c5bfc8ea936ee0020
thayron.matos <thayron.matos@soluti.com.br>
2022-07-11 07:01:02 -0300

    class Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
     </div>
   )
 }
