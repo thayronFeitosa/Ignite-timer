bcb6e27a8e630479cdba8894774fb264e8c0f49f
thayron.matos <thayron.matos@soluti.com.br>
2022-07-12 06:37:43 -0300

    Class -Página: History

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { History } from './pages/History'
+import { History } from './pages/History/index.'
 import { Home } from './pages/Home'
 
 export function Router() {
