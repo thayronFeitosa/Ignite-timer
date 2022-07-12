49a63f864d68361582dff4cb792b864687a85872
thayron.matos <thayron.matos@soluti.com.br>
2022-07-12 06:27:20 -0300

    Class - Finalização da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,9 +15,13 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="text" />
+          <TaskInput type="text" placeholder="Dê um nome para o seu projeto" />
           <label htmlFor="minutesAmount">Durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -27,10 +34,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="button">
+        <StartCountdownButton type="button">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
