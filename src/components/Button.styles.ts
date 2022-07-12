0d34243d86d45ea89d697e7831895b3efafd4de7
thayron.matos <thayron.matos@soluti.com.br>
2022-07-12 03:55:13 -0300

    Class - Cores & fonte

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -19,7 +19,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border-radius: 4px;
   border: 0;
   margin: 8px;
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme['green-500']};
   color: ${props => props.theme.white}
   /* ${props => {
     return css`background-color: ${buttonVariant[props.variant]}`;
