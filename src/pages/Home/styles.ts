e371e1fef7fd2ac85c85a55170331d592718367e
thayron.matos <thayronf3@gmail.com>
2022-07-15 07:34:04 -0300

    Funcionalidades da aplicação - Interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
-  height: 100%;
   flex: 1;
+
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -23,7 +23,7 @@ export const FormContainer = styled.div`
   justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme['gray-100']};
-  font-size: 1.5rem;
+  font-size: 1.125rem;
   font-weight: bold;
   flex-wrap: wrap;
 `
@@ -61,7 +61,7 @@ export const MinutesAmountInput = styled(BaseInput)`
 `
 
 export const CountdownContainer = styled.div`
-  font-family: 'Roboto Mono', nonospace;
+  font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
   line-height: 8rem;
   color: ${(props) => props.theme['gray-100']};
@@ -71,11 +71,10 @@ export const CountdownContainer = styled.div`
 
   span {
     background: ${(props) => props.theme['gray-700']};
-    padding: 2rem;
+    padding: 2rem 1rem;
     border-radius: 8px;
   }
 `
-
 export const Separator = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme['green-500']};
@@ -86,7 +85,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -95,21 +94,32 @@ export const StartCountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
 
   gap: 0.5rem;
   font-weight: bold;
 
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
